fa61b011f1390220fa8a0f8f0fd0b7d127b212db
shashikant <shashikantkamthe77@gmail.com>
2023-11-18 18:21:19 +0530

    Initial

---
+++ src/components/VideoContainer.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react'
+
+const VideoContainer = () => {
+
+  useEffect(()=>{},[]);
+  return (
+    <div>VideoContainer</div>
+  )
+}
+
+export default VideoContainer
